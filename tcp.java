38a79ff7c4cc76f94fa8b54f73cca1246d4c3a98
beinanengg <157193559+beinanengg@users.noreply.github.com>
2024-11-27 23:45:42 +0530

    Create tcp.java

---
+++ tcp.java
@@ -0,0 +1,88 @@
+//Server
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Server70 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = null;
+        Socket s = null;
+
+        try {
+            ss = new ServerSocket(2000);  // Server listens on port 2000
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            try {
+                System.out.println("Server Ready....");
+                s = ss.accept();
+                System.out.println("Client Connected...");
+
+                InputStream istream = s.getInputStream();
+                Scanner fread = new Scanner(new InputStreamReader(istream));
+
+                String fileName = fread.nextLine();
+                System.out.println("Reading contents of " + fileName);
+
+                Scanner contentRead = new Scanner(new FileReader(fileName));
+                OutputStream ostream = s.getOutputStream();
+                PrintWriter pwrite = new PrintWriter(ostream, true);
+
+                while (contentRead.hasNext()) {
+                    pwrite.println(contentRead.nextLine());
+                }
+
+                pwrite.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("File Not Found");
+                OutputStream ostream = s.getOutputStream();
+                PrintWriter pwrite = new PrintWriter(ostream, true);
+                pwrite.println("File Not Found");
+                pwrite.close();            } finally {
+                if (s != null) {
+                    s.close();
+                }
+            }
+        }
+    }
+}
+
+
+//Client
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Client70 {
+    public static void main(String[] args) {
+        Socket s;
+
+        while (true) {
+            try {
+                s = new Socket("172.20.170.29", 2000);  // Connect to the server on port 2000
+                OutputStream ostream = s.getOutputStream();
+                System.out.println("Enter filename");
+                Scanner input = new Scanner(System.in);
+                String fname = input.nextLine();
+
+                PrintWriter pwrite = new PrintWriter(ostream, true);
+                pwrite.println(fname);
+
+                InputStream istream = s.getInputStream();
+                Scanner cRead = new Scanner(new InputStreamReader(istream));
+
+                while (cRead.hasNext()) {
+                    System.out.println(cRead.nextLine());
+                }
+
+                pwrite.close();
+                s.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
